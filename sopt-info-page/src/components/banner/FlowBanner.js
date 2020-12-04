818a5151771fccf708da997eed8cd9320bd91511
Um seoyoung <tjdud0123@naver.com>
2020-12-04 19:38:06 +0900

    반응형 적용 - flow 배너

--- sopt-info-page/src/components/banner/FlowBanner.js
+++ sopt-info-page/src/components/banner/FlowBanner.js
@@ -1,4 +1,4 @@
-import styled, {keyframes, css} from "styled-components";
+import styled, { keyframes, css } from 'styled-components';
 
 const scrolling = keyframes`
     0%{
@@ -10,61 +10,72 @@ const scrolling = keyframes`
 `;
 
 const Marq = styled.div`
-    width: 100%;
-    height:120px;
-    background-color:${(props)=>props.flowstate ? '#FFFFFF':"#8DFF65"};
-    overflow:hidden;
-    display:flex;
-    align-items:center;
+  width: 100%;
+  height: 10vw;
+  background-color: ${props => (props.flowstate ? '#FFFFFF' : '#8DFF65')};
+  overflow: hidden;
+  display: flex;
+  align-items: center;
+  display: flex;
 `;
 
 const MarqUl = styled.ul`
-    list-style:none;
-    height:100%;
-    display: grid;
-    grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr;
-    @media (max-width: 1024px){
-        
-    }   
-    animation: ${scrolling} 12s linear infinite;
+  list-style: none;
+  height: 100%;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr;
+  @media (max-width: 1024px) {
+  }
+  animation: ${scrolling} 12s linear infinite;
 `;
 
 const MarqLi = styled.li`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 33vw;
-    flex-shrink: 0;
-    white-space: nowrap;
-    color: ${(props)=>props.flowstate ? '#8DFF65':'#FFFFFF'};
-    font-size: 30px;
-    @media (max-width: 1024px){
-        font-size: 15px;
-    }   
-    
-    font-weight: 400;
-    line-height: 36px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 33vw;
+  flex-shrink: 0;
+  white-space: nowrap;
+  color: ${props => (props.flowstate ? '#8DFF65' : '#FFFFFF')};
+  font-size: 2vw !important;
+  @media (max-width: 1024px) {
+    font-size: 15px;
+  }
+
+  font-weight: 400;
+  line-height: 36px;
 `;
 
 const bannerMsg1 = 'We make ux/ui design';
 const bannerMsg2 = 'ON SOPT DESIGNER';
 
-function FlowBanner({firstflow}) {
+function FlowBanner({ firstflow }) {
   return (
-    <>  
-        <Marq flowstate={firstflow}>
-            <MarqUl>
-                <MarqLi flowstate={firstflow}>{firstflow ? bannerMsg1:bannerMsg2}</MarqLi>
-                <MarqLi flowstate={firstflow}>{firstflow ? bannerMsg1:bannerMsg2}</MarqLi>
-                <MarqLi flowstate={firstflow}>{firstflow ? bannerMsg1:bannerMsg2}</MarqLi>
-                <MarqLi flowstate={firstflow}>{firstflow ? bannerMsg1:bannerMsg2}</MarqLi>
-                <MarqLi flowstate={firstflow}>{firstflow ? bannerMsg1:bannerMsg2}</MarqLi>
-                <MarqLi flowstate={firstflow}>{firstflow ? bannerMsg1:bannerMsg2}</MarqLi>        
-            </MarqUl>
-        </Marq> 
+    <>
+      <Marq flowstate={firstflow}>
+        <MarqUl>
+          <MarqLi flowstate={firstflow}>
+            {firstflow ? bannerMsg1 : bannerMsg2}
+          </MarqLi>
+          <MarqLi flowstate={firstflow}>
+            {firstflow ? bannerMsg1 : bannerMsg2}
+          </MarqLi>
+          <MarqLi flowstate={firstflow}>
+            {firstflow ? bannerMsg1 : bannerMsg2}
+          </MarqLi>
+          <MarqLi flowstate={firstflow}>
+            {firstflow ? bannerMsg1 : bannerMsg2}
+          </MarqLi>
+          <MarqLi flowstate={firstflow}>
+            {firstflow ? bannerMsg1 : bannerMsg2}
+          </MarqLi>
+          <MarqLi flowstate={firstflow}>
+            {firstflow ? bannerMsg1 : bannerMsg2}
+          </MarqLi>
+        </MarqUl>
+      </Marq>
     </>
   );
 }
-
 
 export default FlowBanner;
