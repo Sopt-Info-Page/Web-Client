8fcbf7b0a8102988469f1901cb7fbc4ab8edeab9
ihatelethargy <naturalflavor1007@naver.com>
2020-12-06 13:40:40 +0900

    소셜 링크

--- sopt-info-page/src/components/Footer.js
+++ sopt-info-page/src/components/Footer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Route, Link } from 'react-router-dom';
 import styled from 'styled-components';
 import OvalBtn from './buttons/OvalBtn';
 
@@ -18,6 +19,9 @@ const SocialWrapper = styled.div`
   align-items: flex-end;
   margin-left: 50px;
   margin-bottom: 100px;
+  a {
+    text-decoration: none;
+  }
 `;
 
 const LogoWrapper = styled.div`
@@ -47,39 +51,53 @@ const Footer = props => (
   <>
     <FooterWrapper>
       <SocialWrapper>
-        <OvalBtn
-          border_color={'#FFFFFF'}
-          border_width={1}
-          background={'#FFFFFF'}
-          color={'#FF0404'}
-          height={50}
-          width={120}
-          font_size={'12px'}
-          text={'YouTube'}
-          className="mr-3 c-pointer"
-        ></OvalBtn>
-        <OvalBtn
-          border_color={'#FFFFFF'}
-          border_width={1}
-          background={'#FFFFFF'}
-          color={'#0C41FF'}
-          height={50}
-          width={120}
-          font_size={'12px'}
-          className="mr-3 c-pointer"
-          text={'Facebook'}
-        ></OvalBtn>
-        <OvalBtn
-          border_color={'#FFFFFF'}
-          border_width={1}
-          background={'#FFFFFF'}
-          color={'#000'}
-          height={50}
-          width={120}
-          font_size={'12px'}
-          className="c-pointer"
-          text={'sopt.org'}
-        ></OvalBtn>
+        <a
+          href={'https://www.youtube.com/channel/UCui_xDNrVlxAuGJUV8zmN6A'}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <OvalBtn
+            border_color={'#FFFFFF'}
+            border_width={1}
+            background={'#FFFFFF'}
+            color={'#FF0404'}
+            height={50}
+            width={120}
+            font_size={'12px'}
+            text={'YouTube'}
+            className="mr-3 c-pointer"
+          ></OvalBtn>
+        </a>
+        <a
+          href={'https://www.facebook.com/clubsopt/'}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <OvalBtn
+            border_color={'#FFFFFF'}
+            border_width={1}
+            background={'#FFFFFF'}
+            color={'#0C41FF'}
+            height={50}
+            width={120}
+            font_size={'12px'}
+            className="mr-3 c-pointer"
+            text={'Facebook'}
+          ></OvalBtn>
+        </a>
+        <a href={'http://sopt.org/'} target="_blank" rel="noopener noreferrer">
+          <OvalBtn
+            border_color={'#FFFFFF'}
+            border_width={1}
+            background={'#FFFFFF'}
+            color={'#000'}
+            height={50}
+            width={120}
+            font_size={'12px'}
+            className="c-pointer"
+            text={'sopt.org'}
+          ></OvalBtn>
+        </a>
       </SocialWrapper>
       <LogoWrapper>
         <Logo></Logo>
